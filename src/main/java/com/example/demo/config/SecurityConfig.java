bb04a6ea2ad86ea132b8305207fc210a359fb1ac
Pranavpeddinti123 <163733336+Pranavpeddinti123@users.noreply.github.com>
2025-09-23 22:19:15 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173,"http://ec2-13-218-220-63.compute-1.amazonaws.com:5173"")
+                        .allowedOrigins("http://localhost:5173","http://ec2-13-218-220-63.compute-1.amazonaws.com:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
